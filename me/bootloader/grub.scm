7d7e625108e48839b7bc2ac1fcee00f31695df22
Jiwei Yang <yangjiwei@protonmail.com>
2023-02-25 13:31:19 +1000

    1

--- me/bootloader/grub.scm
+++ me/bootloader/grub.scm
@@ -19,10 +19,48 @@
   #:use-module (me packages bootloaders)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-2)
-  #:export (grub-efi-luks2-bootloader))
+  #:export (grub-efi-luks2-bootloader
+            grub-efi-bootloader-luks2))
 
 
 (define grub-efi-luks2-bootloader
   (bootloader
    (inherit grub-efi-bootloader)
    (package grub-efi-luks2)))
+
+
+(define install-grub-efi-mkimage
+  ;; "Create an Grub EFI image with included cryptomount support for luks2,
+;; which grub-install does not handle yet."
+  #~(lambda (bootloader efi-dir mount-point)
+        (when efi-dir
+            (let ((grub-mkimage (string-append bootloader "/bin/grub-mkimage"))
+                  ;; Required modules, YMMV.
+                  (modules (list "luks2" "part_gpt" "cryptodisk" "gcry_rijndael" "pbkdf2" "gcry_sha256" "gcry_sha512" "btrfs"))
+                  (prefix (string-append mount-point "/root/harden/boot/grub"))  ; btrfs subvol root
+                  ;; Different configuration required to set up a crypto
+                  ;; device. Change crypto_uuid to match your output of
+                  ;; `cryptsetup luksUUID /device`.
+                  ;; XXX: Maybe cryptomount -a could work?
+                  (config #$(plain-file "grub.cfg" "set crypto_uuid=3758ac97d5214d80adcad19d4bc57b88
+cryptomount -u $crypto_uuid
+set root=crypto0
+set prefix=($root)/root/harden/boot/grub
+insmod normal
+normal"))
+                  (target-esp (if (file-exists? (string-append mount-point efi-dir))
+                                  (string-append mount-point efi-dir)
+                                  efi-dir)))
+              (apply invoke (append
+                             (list
+                               grub-mkimage
+                              "-p" prefix
+                              "-O" "x86_64-efi"
+                              "-c" config
+                              "-o" (string-append target-esp "/EFI/Guix/grubx64.efi"))
+                             modules))))))
+(define grub-efi-bootloader-luks2
+  (bootloader
+    (inherit grub-efi-bootloader)
+    (name 'grub-efi-luks2)
+    (installer install-grub-efi-mkimage)))
